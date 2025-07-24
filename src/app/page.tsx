1f14865f51e047c65787f998e19023fb37d17f87
Sainudheen P <sainudheen2003@gmail.com>
2025-07-24 12:41:35 +0530

    Fix: Popover issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/navbar";
 import StatCards from "@/components/stats-cards";
-import TasksArea from "@/components/tasks-area/search-input";
-
+import TasksArea from "@/components/tasks-area/task-area";
 export default function Home() {
   return (
     <div className={`poppins min-h-screen `}>
