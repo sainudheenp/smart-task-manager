87f4c3a0cd230be12ff9d923df03215cc3f3c7ad
Sainudheen P <sainudheen2003@gmail.com>
2025-07-30 23:21:50 +0530

    feat: enhance task API with user authentication for GET and POST requests

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,24 +1,33 @@
 import { prisma } from '@/lib/prisma'
+import { auth } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server'
 
 // GET all tasks
 export async function GET() {
+  const session = await auth();
+  const userId = session?.userId;
+  if (!userId) {
+    return NextResponse.json([], { status: 401 });
+  }
   const tasks = await prisma.task.findMany({
+    where: { userId },
     orderBy: { createdAt: 'desc' }
-  })
-  return NextResponse.json(tasks)
+  });
+  return NextResponse.json(tasks);
 }
 
 // POST new task
 export async function POST(req: Request) {
-  const body = await req.json()
-
-  const { title, label, priority } = body
-
-  if (!title || !label || !priority) {
-    return NextResponse.json({ error: 'Missing fields' }, { status: 400 })
+  const session = await auth();
+  const userId = session?.userId;
+  if (!userId) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
-
+  const body = await req.json();
+  const { title, label, priority } = body;
+  if (!title || !label || !priority) {
+    return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
+  }
   const newTask = await prisma.task.create({
     data: {
       title,
@@ -26,8 +35,8 @@ export async function POST(req: Request) {
       isFavorite: false,
       priority,
       status: 'Todo',
+      userId,
     },
-  })
-
-  return NextResponse.json(newTask)
+  });
+  return NextResponse.json(newTask);
 }
