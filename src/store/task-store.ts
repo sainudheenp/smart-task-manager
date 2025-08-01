d64404ab08ef009c4f02ae723f9be07a4fd5747b
Sainudheen P <sainudheen2003@gmail.com>
2025-08-01 22:01:53 +0530

    store

--- src/store/task-store.ts
+++ src/store/task-store.ts
@@ -1,14 +1,17 @@
 import { create } from 'zustand';
 
-// Example: Task state store
 interface TaskState {
   tasks: any[];
   setTasks: (tasks: any[]) => void;
   addTask: (task: any) => void;
+  filter: string;
+  setFilter: (filter: string) => void;
 }
 
 export const useTaskStore = create<TaskState>((set) => ({
   tasks: [],
   setTasks: (tasks) => set({ tasks }),
   addTask: (task) => set((state) => ({ tasks: [task, ...state.tasks] })),
+  filter: '',
+  setFilter: (filter) => set({ filter }),
 }));
